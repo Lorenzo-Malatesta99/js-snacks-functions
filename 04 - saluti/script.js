5ad43175a0d696ecbc9c75163384fd867a48f66a
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-10-21 16:52:55 +0200

    ADD ex 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
 
+const saluto = (sal) => {
+  return "Buongiorno " + sal + "!";
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluto(name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

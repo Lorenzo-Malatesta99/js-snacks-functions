ed089dbeda82d3429ac0b253246ce1a0ed01b4d9
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-10-21 16:24:24 +0200

    ADD ex 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function atLetter(array, lett) {
+    let lettera = String(lett);
+    for (let i = 0; i < array.length; i++){
+        if (array[i].charAt(0) === lettera) {
+            console.log(array[i]);
+        }
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(atLetter(names, "A"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
